4d25c4d2fc0af5ae03722ef0ce037e1a391b5468
nelson360 <nelsonomondi99@gmail.com>
2022-11-25 15:56:41 +0300

    i

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 
-set -e
+set - e
 
 npm run build
 
 cd dist
 
 git init
+
 git add -A
+
 git commit -m 'deploy production'
-git push -f git@github.com:nelson360/nelson360.github.io.git main
+
+git push -f git@github.com:nelson360/nelson360.git master:gh-pages
 
 cd -
